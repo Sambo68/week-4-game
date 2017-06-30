3df332908f3aae413c9b7e649f4c692188a6cc30
Administrator <isamamireh@c02g31vudf91.attlocal.net>
2017-06-30 19:06:20 -0400

    modification and added assets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,16 @@ var wins = 0;
 var losses = 0;
 //current scores
 var addition = 0;
-//selects a random number from 14 to 120
-var total = Math.floor((Math.random()* 100) + 19);
+//selects a random number from 5 to 80
+var total = Math.floor((Math.random()* 80) + 5);
 //assigns random value to stars from 1-13
 var blue = Math.floor((Math.random()* 12) + 1);
 var green = Math.floor((Math.random()* 12) + 1);
 var red = Math.floor((Math.random()* 12) + 1);
 var yellow = Math.floor((Math.random()* 12) + 1);
-//function that updates the additions in html
+var audioloss = new Audio('assets/Audio/Sad_Trombone-Joe_Lamb-665429450.mp3')
+var audiowin = new Audio('assets/Audio/Ta Da-SoundBible.com-1884170640.mp3')
+//A function that is assigned to a variable that updates the additions in html
 var updateAddition = function (){
 	$('.addition').empty();
 	$('.addition').append(addition);
@@ -22,7 +24,7 @@ var updateAddition = function (){
 //restart function
 var restart = function(){
 	addition = 0;
-    total = Math.floor((Math.random()* 100) + 19);
+    total = Math.floor((Math.random()* 80) + 5);
 
 	$('.total').empty();
 	$('.total').append(total);
@@ -39,10 +41,12 @@ var logic = function (){
 	if (addition == total) {
 		wins = wins + 1;
 		
+		audiowin.play();
 		restart();
 	}else if(addition > total){
 		losses = losses + 1;
 		
+		audioloss.play();
 		restart();
 	}else{
 		updateAddition();
